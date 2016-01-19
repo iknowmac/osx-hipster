30cdda8e1de2c27f9aa8fa60882cc42c85cbe3c6
Terry Owen <terry@iknowmac.com>
2016-01-19 14:54:13 -0700

    Initial Commit

---
+++ hIPster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hIPster
+//
+//  Created by Terry Owen on 1/19/16.
+//  Copyright © 2016 iKnowMac, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hIPster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hIPster
+//
+//  Created by Terry Owen on 1/19/16.
+//  Copyright © 2016 iKnowMac, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
